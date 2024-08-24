46f0fc28b67dd2afe5807f745712bb087efb18a8
parsa <parsajavidi17@gmail.com>
2024-08-24 14:06:39 -0700

    test news model

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+@admin.register(models.News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["title","author","body","created"]
+
+
+admin.site.register(models.Category)
--- news/models.py
+++ news/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.title
+
 class News(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="news")
+    category = models.ManyToManyField(Category, related_name="news",default="1")
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
 
     class Meta:
         ordering = ["created","updated"]
