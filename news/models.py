2fb9206321f3b2d118cded9543005a37ea79b186
parsa <parsajavidi17@gmail.com>
2024-08-24 13:52:50 -0700

    create news  models

--- news/models.py
+++ news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="news")
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created","updated"]
+
